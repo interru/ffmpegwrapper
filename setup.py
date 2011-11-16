917fce3b213ae98655362b9f83a5c273a1bb930c
Mathias Koehler <mail@mathias.im>
2011-11-16 21:05:32 +0100

    add some informations in the setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name="ffmpegwrapper",
     version="0.1-dev",
+    license="BSD",
+    zip_safe=False,
+    author="Mathias Koehler",
+    author_email="mail@mathias.im",
+    url="http://github.com/interrupted/ffmpegwrapper",
+    description='A simple wrapper for ffmpeg-cli',
     packages=['ffmpegwrapper']
 )
